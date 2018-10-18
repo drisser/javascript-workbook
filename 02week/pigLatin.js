9faee22f2aeb7f4c0d77c9ba566388e6bccd457d
Darwin <risser.darwin@gmail.com>
2018-10-18 14:18:03 -0500

    init push

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,13 +7,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//CODE PLAN
+//check if input is valid 'isWord' 
+//if falsey, return 'please enter a word'
+//if truthy, check to see if the first letter of the word is a vowel
+//if truthy, return the original string + 'yay'
+//if falsey, convert string to an array 'arrWord'
+//loop through the array comparing each letter against 5 vowels
+//after finding the first vowel, shift the letters before it to the end of the array
+//add 'ay' to the end of the array
+//convert the new array back to a string
+//return the new string
 
-function pigLatin(word) {
+const isWord = (word) => typeof word === 'string'; //this function checks if the user inputs a word
 
-  // Your code here
+const arrWord = (word) => word.split(''); //this function converts the string to an array
 
+const pigLatin = (word) => {
+  if (isWord(word)){
+    if (word[0] === 'a' || word[0] === 'e' || word[0] === 'i' || word[0] === 'o' || word[0] === 'u'){
+      return word + 'yay' //this function checks to see if the first letter of the string is a vowel, if truthy, it returns the word in pig latin
+    } else {
+      const newArr = arrWord(word);
+    } //this is where I got stuck. After doing lots of googling, I still couldn't figure it out, I think understand how to manipulate the different parts-
+      //of the array (using shift and push?) but I couldn't figure out how to loop through the array and then do something after the loop found the first vowel
+  } return 'please enter a word'
 }
-
 
 function getPrompt() {
   rl.question('word ', (answer) => {
